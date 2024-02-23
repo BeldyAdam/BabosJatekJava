f88741b7d2d7a4fcff748c40610ba483f73e3b84
BeldyAdam <beldy.adam@gmail.com>
2024-02-23 22:47:42 +0100

    A babok száma nem mehet negatívba

--- src/babosjatek/BabosJatek.java
+++ src/babosjatek/BabosJatek.java
@@ -3,7 +3,6 @@ package babosjatek;
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class BabosJatek {
     
     static Random rnd = new Random();
@@ -19,7 +18,7 @@ public class BabosJatek {
     private static int JatekosLepes() {
         System.out.println("Hány babot szeretnél elvenni?");
         jLepesSzam = sc.nextInt();
-        while(jLepesSzam > 2){
+        while (jLepesSzam > 2 || jLepesSzam <= 0 || jLepesSzam > babok) {
             System.out.println("Hibás lépés");
             jLepesSzam = sc.nextInt();
         }
@@ -27,25 +26,27 @@ public class BabosJatek {
     }
 
     private static int GepLepes() {
-        gLepesSzam = rnd.nextInt(1,3);
+        gLepesSzam = rnd.nextInt(1, 3);
         System.out.printf("Gép lépése %d\n", gLepesSzam);
         return gLepesSzam;
     }
 
     private static void Jatek(int szam) {
         babok = szam;
-        while(babok != 0 && babok > 0){
+        while (babok > 0) {
+            System.out.println("Hátralévő babok: "+babok);
             babok -= JatekosLepes();
-            if(babok <= 0){
+            if (babok <= 0) {
                 System.out.println("Játékos győzött");
-            }else{
-                 babok -= GepLepes();
-                 if(babok <= 0){
-                     System.out.println("G győzött");
-                 }
+                return;
+            } else {
+                babok -= GepLepes();
+                if (babok <= 0) {
+                    System.out.println("Gép győzött");
+                    return;
+                }
             }
         }
-
+        System.out.println("Negatív babok száma!");
     }
-    
 }
